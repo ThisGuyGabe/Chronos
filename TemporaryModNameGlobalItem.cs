fe695545060cf16bf7d26d79877620f8bc35fc75
ThatGuyGabe <gabrielnovoa2021@outlook.com>
2024-01-05 14:16:38 -0500

    Literally Everything So far
    This is everything so far just the bunny cannon and the regular cannon but with portability.

---
+++ TemporaryModNameGlobalItem.cs
@@ -0,0 +1,26 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TemporaryModName 
+{
+	public class GenericGlobalItem : GlobalItem
+	{
+		public override void SetDefaults(Item item)
+		{
+			item.autoReuse = true;
+
+			if(item.type == ItemID.Cannonball)
+			{
+				item.ammo = ItemID.Cannonball;
+				item.shoot = ProjectileID.CannonballFriendly;
+			}
+
+			if(item.type == ItemID.ExplosiveBunny)
+			{
+				item.ammo = ItemID.ExplosiveBunny;
+				item.shoot = ProjectileID.ExplosiveBunny;
+			}
+		}
+	}
+}
