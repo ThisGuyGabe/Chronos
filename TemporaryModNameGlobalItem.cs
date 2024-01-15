b078f771a20b2afc689c3967c69250ad3209d64d
ThatGuyGabe <gabrielnovoa2021@outlook.com>
2024-01-15 13:11:40 -0500

    Just a small change to global item.

--- TemporaryModNameGlobalItem.cs
+++ TemporaryModNameGlobalItem.cs
@@ -6,19 +6,6 @@ namespace TemporaryModName
 {
 	public class GenericGlobalItem : GlobalItem
 	{
-		public override void SetDefaults(Item item)
-		{
-			if(item.type == ItemID.Cannonball)
-			{
-				item.ammo = ItemID.Cannonball;
-				item.shoot = ProjectileID.CannonballFriendly;
-			}
 
-			if(item.type == ItemID.ExplosiveBunny)
-			{
-				item.ammo = ItemID.ExplosiveBunny;
-				item.shoot = ProjectileID.ExplosiveBunny;
-			}
-		}
 	}
 }
