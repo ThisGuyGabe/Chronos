0897a6d4cb017cceb2218f73ef2698e293b174d1
ThatGuyGabe <gabrielnovoa2021@outlook.com>
2024-01-22 14:45:38 -0500

    The Aquatic Vanity Set
    Added the Aquatic Vanity Set and added a small version of the icon and a version of the icon for the workshop.

---
+++ Content/Items/Armor/Vanity/AquaticDiverSet/AquaticBoots.cs
@@ -0,0 +1,29 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace Chronos.Content.Items.Armor.Vanity.AquaticDiverSet
+{
+	[AutoloadEquip(EquipType.Legs)]
+	public class AquaticBoots : ModItem
+	{
+        public sealed override string Texture => "Chronos/Assets/Textures/Items/Armor/Vanity/AquaticDiverSet/AquaticBoots";
+        public override void SetDefaults() 
+		{
+			Item.width = 22;
+			Item.height = 18;
+			Item.value = Item.sellPrice(silver: 45);
+			Item.rare = ItemRarityID.Green;
+			Item.vanity = true;
+		}
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.Silk, 20)
+                .AddIngredient(ItemID.Coral, 3)
+                .AddIngredient(ItemID.Seashell, 1)
+                .AddTile(TileID.Loom)
+                .Register();
+        }
+    }
+}
---
+++ Content/Items/Armor/Vanity/AquaticDiverSet/AquaticHood.cs
@@ -0,0 +1,29 @@
+﻿using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Chronos.Content.Items.Armor.Vanity.AquaticDiverSet
+{
+    [AutoloadEquip(EquipType.Head)]
+    public class AquaticHood : ModItem
+    {
+        public sealed override string Texture => "Chronos/Assets/Textures/Items/Armor/Vanity/AquaticDiverSet/AquaticHood";
+        public override void SetDefaults()
+        {
+            Item.width = 32;
+            Item.height = 34;
+            Item.value = Item.sellPrice(silver: 75);
+            Item.rare = ItemRarityID.Green;
+            Item.vanity = true;
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.Silk, 20)
+                .AddIngredient(ItemID.Coral, 3)
+                .AddIngredient(ItemID.Seashell, 1)
+                .AddTile(TileID.Loom)
+                .Register();
+        }
+    }
+}
