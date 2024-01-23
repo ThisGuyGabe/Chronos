336f48a4b106a03338d4550d4de4051c7438f2a2
ThatGuyGabe <gabrielnovoa2021@outlook.com>
2024-01-23 16:42:05 -0500

    build.txt changes and localization stuff
    The title says it all basically.
    
    Co-Authored-By: Stranza <115468538+stranza@users.noreply.github.com>

--- Content/Dusts/GlowLine.cs
+++ Content/Dusts/GlowLine.cs
@@ -4,50 +4,57 @@ using ReLogic.Content;
 using Terraria;
 using Terraria.ModLoader;
 
-namespace Chronos.Content.Dusts;
+namespace Chronos.Content.Dusts
+{
+    public class GlowLine : ModDust
+    {
+        public sealed override string Texture => "Chronos/Assets/Textures/Dusts/GlowLine";
 
-public sealed class GlowLine : ModDust {
-    public sealed override string Texture => "Chronos/Assets/Textures/Dusts/GlowLine";
-    public override Color? GetAlpha(Dust dust, Color lightColor) {
-		if (dust.fadeIn <= 2)
-			return Color.Transparent;
+        public override Color? GetAlpha(Dust dust, Color lightColor)
+        {
+            if (dust.fadeIn <= 2)
+                return Color.Transparent;
 
-		return dust.color * MathHelper.Min(1, dust.fadeIn / 20f);
-	}
+            return dust.color * MathHelper.Min(1, dust.fadeIn / 20f);
+        }
 
-	public override void OnSpawn(Dust dust) {
-		dust.fadeIn = 0;
-		dust.noLight = false;
-		dust.frame = new Rectangle(0, 0, 8, 128);
-		dust.customData = dust.scale;
+        public override void OnSpawn(Dust dust)
+        {
+            dust.fadeIn = 0;
+            dust.noLight = false;
+            dust.frame = new Rectangle(0, 0, 8, 128);
+            dust.customData = dust.scale;
 
-		dust.shader = new Terraria.Graphics.Shaders.ArmorShaderData(new Ref<Effect>(ModContent.Request<Effect>("Chronos/Effects/GlowingDust", AssetRequestMode.ImmediateLoad).Value), "GlowingDustPass");
-	}
+            dust.shader = new Terraria.Graphics.Shaders.ArmorShaderData(new Ref<Effect>(ModContent.Request<Effect>("Chronos/Effects/GlowingDust", AssetRequestMode.ImmediateLoad).Value), "GlowingDustPass");
+        }
 
-	public override bool Update(Dust dust) {
-		if ((float)dust.customData != 0f) {
-			//dust.position -= new Vector2(4, 64) * dust.scale;
-			dust.scale = (float)dust.customData;
-			dust.customData = 0f;
-		}
+        public override bool Update(Dust dust)
+        {
+            if ((float)dust.customData != 0f)
+            {
+                //dust.position -= new Vector2(4, 64) * dust.scale;
+                dust.scale = (float)dust.customData;
+                dust.customData = 0f;
+            }
 
-		dust.rotation = dust.velocity.ToRotation() + 1.57f;
-		dust.position += dust.velocity;
+            dust.rotation = dust.velocity.ToRotation() + 1.57f;
+            dust.position += dust.velocity;
 
-		dust.velocity *= 0.98f;
-		dust.color *= 0.97f;
+            dust.velocity *= 0.98f;
+            dust.color *= 0.97f;
 
-		if (dust.fadeIn <= 2)
-			dust.shader.UseColor(Color.Transparent);
-		else
-			dust.shader.UseColor(dust.color);
+            if (dust.fadeIn <= 2)
+                dust.shader.UseColor(Color.Transparent);
+            else
+                dust.shader.UseColor(dust.color);
 
-		dust.fadeIn++;
+            dust.fadeIn++;
 
-		Lighting.AddLight(dust.position, dust.color.ToVector3() * 0.6f);
+            Lighting.AddLight(dust.position, dust.color.ToVector3() * 0.6f);
 
-		if (dust.fadeIn > 60)
-			dust.active = false;
-		return false;
-	}
+            if (dust.fadeIn > 60)
+                dust.active = false;
+            return false;
+        }
+    }
 }
